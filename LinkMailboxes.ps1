51cf9d675587248ec81a9fc2db723a14e182fbb0
DemCoders <57455153+DemCoders@users.noreply.github.com>
2021-03-17 18:12:37 -0400

    linked mailboxes
    script to change native mailboxes to linked mailboxes

---
+++ LinkMailboxes.ps1
@@ -0,0 +1,42 @@
+﻿
+$allmbx = get-mailbox -ResultSize unlimited | ? {$_.identity -notLike "*Discovery*" -and $_.identity -notlike "*Demetrios*" -and $_.alias -notlike "a360*"}
+
+$emea = "OU=EMEAUsers,DC=EMEA,DC=authA360Labs,DC=com"
+$na = "OU=NAEastUsers,DC=NAEAST,DC=authA360Labs,DC=com"
+$paths += @($emea,$na)
+
+$emeaDC = "emeadc1.emea.autha360labs.com"
+$naDC = "nadc1.naeast.autha360labs.com"
+
+
+$allmbx | % {
+
+$error.Clear()
+write-host "Processing......"
+write-host -ForegroundColor yellow "disabling $_ local mailbox "
+Disable-Mailbox -Identity $_.identity -Confirm:$false
+$path = get-random $paths -Count 1
+Set-ADUser $_.Alias -Enabled:$false
+write-host -ForegroundColor yellow "re-enabling  $_ in $path  "
+
+try{
+    if($path -like "OU=EMEA*"){
+                
+                $linkedAccount = $_.Alias + "@emea.autha360labs.com"
+                Connect-Mailbox -Identity $_.displayName -LinkedDomainController $emeaDC -LinkedMasterAccount $linkedAccount -Database $_.database -User $_.displayName
+            
+            }
+            Else {
+
+                $linkedAccount = $_.Alias + "@naeast.autha360labs.com"
+                Connect-Mailbox -Identity $_.displayName -LinkedDomainController $naDC -LinkedMasterAccount $linkedAccount -Database $_.database -User $_.displayName
+
+                }
+        }
+    Catch{throw "something went wrong" ; $error}
+
+    
+}
+
+
+
