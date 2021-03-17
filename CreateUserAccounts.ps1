9802f989d9cebf49098ec5864f1f9f60de4a9091
DemCoders <57455153+DemCoders@users.noreply.github.com>
2021-03-17 15:49:58 -0400

    New Script to create bulk accounts
    Using a source file (not included here)

---
+++ CreateUserAccounts.ps1
@@ -0,0 +1,21 @@
+﻿$emea = "OU=EMEAUsers,DC=EMEA,DC=authA360Labs,DC=com"
+$na = "OU=NAEastUsers,DC=NAEAST,DC=authA360Labs,DC=com"
+#$mbxpwd = Get-Credential
+$csv = import-csv C:\dev\EmeaAllUsers.csv
+$path = $emea
+
+$csv | % {
+
+$displayName = $_.DisplayName
+$name = $_.Name
+$PrimaryEmailAddress = $_.PrimarySmtp
+$upn = $_.upn
+    
+    New-ADUser -Name $displayName  -DisplayName $displayName  -EmailAddress $PrimaryEmailAddress `
+    -AccountPassword $mbxpwd.password -ChangePasswordAtLogon:$false -PasswordNeverExpires:$true `
+    -Path $path -Enabled:$True -UserPrincipalName $upn
+    
+}
+
+
+
